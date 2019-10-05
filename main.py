aafbeb7d779864c6c8824c2a5637009387ede472
hyuntak03 <hyuntak03.lee@gmail.com>
2019-10-05 21:30:16 +0900

    modified

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import turtle as t
 import math
-b = t.Turtle()
 angle = 0
 v = 0
 vx = 0;
@@ -11,16 +10,8 @@ g = 9.8
 tm = 0.3
 distance = 0;
 height = 0;
+
 def draw_pos(x,y):
-    b.hideturtle()
-    distance = t.numinput("입력", "거리:", 200, 50, 600)
-    height = t.numinput("입력", "높이:", 150, 100, 200)
-    b.penup()
-    b.setpos(distance - 200, 150)
-    b.pendown()
-    b.left(90)
-    b.forward(height - 60)
-    b.circle(30)
     v = t.numinput("입력","속력:",50,10,100)
     angle = math.radians(t.numinput("입력","각도:",45,0,360))
     t.hideturtle()
@@ -32,7 +23,6 @@ def draw_pos(x,y):
     hl = -(t.window_height() / 2)
     ux = v * math.cos(angle)
     uy = v * math.sin(angle)
-
     while True:
         uy = uy + (-1*g)*tm
         dy = t.ycor() + uy - (g*tm)
@@ -48,7 +38,6 @@ t.shape('circle')
 t.shapesize(0.5,0.5,0)
 t.speed(1)
 t.penup()
-t.goto(-200,150)
 s = t.Screen()
 s.onscreenclick(draw_pos)
 s.listen()
